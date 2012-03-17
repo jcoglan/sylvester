9a80c8ea3f6e2d90bfad48b907024639432e86ac
James Coglan <jcoglan@gmail.com>
2012-03-17 13:28:43 +0000

    Run on WSH.

--- test/console.js
+++ test/console.js
@@ -1,10 +1,23 @@
+if (this.ActiveXObject)
+  load = function(path) {
+    var fso = new ActiveXObject('Scripting.FileSystemObject'), file, runner
+    try {
+      file   = fso.OpenTextFile(path)
+      runner = function() { eval(file.ReadAll()) }
+      runner()
+    } finally {
+      try { if (file) file.Close() } catch (e) {}
+    }
+  }
+
+JSCLASS_PATH = 'node_modules/jsclass/src'
+
 if (typeof require === 'function') {
   Sylvester = require('../lib/sylvester')
   require('jsclass')
   require('./runner')
 }
 else {
-  JSCLASS_PATH = 'node_modules/jsclass/src'
   load('lib/sylvester.js')
   load(JSCLASS_PATH + '/loader.js')
   load('test/runner.js')
